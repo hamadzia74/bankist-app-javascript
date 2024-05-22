0dfab9522ddb684a5b096d27bca42c77b03905b5
Hamad Zia <hamadzia74@gmail.com>
2024-05-22 12:35:21 -0700

    Added the additional check to sort the data perfectly

--- script.js
+++ script.js
@@ -187,9 +187,11 @@ btnClose.addEventListener('click', function (e) {
   inputCloseUsername.value = inputClosePin.value = '';
 });
 
+let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, true);
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 
 const currencies = new Map([
