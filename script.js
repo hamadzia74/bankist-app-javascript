9b92234de8fd901d0870a75e170f200d14f652d6
Hamad Zia <hamadzia74@gmail.com>
2024-05-22 12:22:30 -0700

    Worked on Implementing the Sorting Data Functionality

--- script.js
+++ script.js
@@ -59,10 +59,11 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
@@ -161,7 +162,7 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
-btnLoan.addEventListener('click', function(e) {
+btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
   const amount = Number(inputLoanAmount.value);
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
@@ -169,7 +170,7 @@ btnLoan.addEventListener('click', function(e) {
     updateUI(currentAccount);
   }
   inputLoanAmount.value = '';
-})
+});
 
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
@@ -184,6 +185,11 @@ btnClose.addEventListener('click', function (e) {
     containerApp.style.opacity = 0;
   }
   inputCloseUsername.value = inputClosePin.value = '';
+});
+
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, true);
 });
 
 const currencies = new Map([
