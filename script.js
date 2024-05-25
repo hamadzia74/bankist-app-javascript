a0e9c760745bca8abecb19a7fb56e93a6679a445
Hamad Zia <hamadzia74@gmail.com>
2024-05-25 08:05:57 -0700

    Implemented Operations on Dates

--- script.js
+++ script.js
@@ -48,10 +48,10 @@ const account1 = {
     '2023-12-23T07:42:02.383Z',
     '2024-01-28T09:15:04.904Z',
     '2024-04-01T10:17:24.185Z',
-    '2024-05-08T14:11:59.604Z',
-    '2024-05-27T17:01:17.194Z',
-    '2024-07-11T23:36:17.929Z',
-    '2024-07-12T10:51:36.790Z',
+    '2024-05-22T14:11:59.604Z',
+    '2024-05-23T17:01:17.194Z',
+    '2024-05-24T23:36:17.929Z',
+    '2024-05-25T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -67,11 +67,11 @@ const account2 = {
     '2023-11-01T13:15:33.035Z',
     '2023-11-30T09:48:16.867Z',
     '2023-12-25T06:04:23.907Z',
-    '2024-01-25T14:18:46.235Z',
-    '2024-02-05T16:33:06.386Z',
-    '2024-04-10T14:43:26.374Z',
-    '2024-06-25T18:49:59.371Z',
-    '2024-07-26T12:01:20.894Z',
+    '2024-05-25T14:18:46.235Z',
+    '2024-05-22T16:33:06.386Z',
+    '2024-05-23T14:43:26.374Z',
+    '2024-05-24T18:49:59.371Z',
+    '2024-05-25T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -104,6 +104,22 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -112,12 +128,9 @@ const displayMovements = function (acc, sort = false) {
     : acc.movements;
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
-
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
+
     const html = `
     <div class="movements__row">
           <div class="movements__type movements__type--${type}">${
